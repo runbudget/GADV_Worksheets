9e6b77743c931741664e7c9bf79852eb115de0f3
runbudget <162553866+runbudget@users.noreply.github.com>
2025-05-05 14:29:33 +0800

    namespace for gadv week 3 folder 1

---
+++ Assets/Worksheets/W03_ unity scripting/Basic Scripting/Namespaces/Folder1/DemoScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectNameSpace1
+{
+    public class DemoScript : MonoBehaviour
+    {
+        //other code
+    }
+}
+
+// get rid of the other classes
